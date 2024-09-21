57a7af46d705ac7d2a675957e59be101f4b0f08d
“Sahithya <sahithya.hanumandla01@gmail.com>
2024-09-20 22:39:00 -0400

    scaffolding the project,Basic features implemented

---
+++ src/app/components/Messages.tsx
@@ -0,0 +1,25 @@
+import { type Message as TMessage} from "ai/react"
+import Message from "./Message"
+import { MessageSquare } from "lucide-react"
+
+
+interface MessageProps{
+    messages:TMessage[]
+}
+
+const Messages = ({messages}:MessageProps) => {
+  return (
+    <div className="flex max-h-[calc(100vh-3.5rem-7rem)] flex-1 flex-col overflow-y-auto">
+      {messages? messages.map((message,i)=>(
+        <Message key={i} content={message.content} isUserMessage={message.role === "user"}/>
+      ))
+      :
+      (<div className="flex-1 flex flex-col items-center hustify-center gap-2">
+        <MessageSquare className="size-8 text-blue-500"/>
+        <h3 className="font-semibold text-xl">You're all set!</h3>
+      </div>)}
+    </div>
+  )
+}
+
+export default Messages
